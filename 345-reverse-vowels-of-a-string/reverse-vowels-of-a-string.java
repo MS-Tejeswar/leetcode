6ddf8765b8dabf8cdf4b0818962b8629842a03ad
Madduri satya Tejeswar <81846238+MS-Tejeswar@users.noreply.github.com>
2024-09-14 18:30:05 +0530

    Time: 266 ms (5.16%) | Memory: 46.4 MB (5.87%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -6,16 +6,21 @@ class Solution {
         vow.add('i');
         vow.add('o');
         vow.add('u');
+        vow.add('A');
+        vow.add('E');
+        vow.add('I');
+        vow.add('O');
+        vow.add('U');
 
         List<Character> new_vow=new ArrayList<>();
         for(int i=0;i<s.length();i++){
-            if(vow.contains(Character.toLowerCase(s.charAt(i))))
+            if(vow.contains(s.charAt(i)))
             new_vow.add(s.charAt(i));
         }
         int ind=new_vow.size()-1;
         String f="";
         for(int i=0;i<s.length();i++){
-            if(vow.contains(Character.toLowerCase(s.charAt(i)))){
+            if(vow.contains(s.charAt(i))){
                 f+=new_vow.get(ind--);
                 
             }
